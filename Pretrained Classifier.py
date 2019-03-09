e738416ed9c039bc5fe6c2675a3378aa6d9f5653
Dwight foster <dwightfoster03@gmail.com>
2019-03-09 15:29:17 -0500

    Added validation set
    I added a validation set to the training loop so you can see how well the model is doing at predicting before you go through the test loop.

--- Pretrained Classifier.py
+++ Pretrained Classifier.py
@@ -8,17 +8,19 @@ import torch.optim as optim
 from torch.utils.data.sampler import SubsetRandomSampler
 
 #Defining the test and train loaders
-train_data = datasets.ImageFolder(r'Drive/asl-alphabet-test', transform=transforms.Compose([transforms.RandomResizedCrop(224),transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                                                                                                                std=[0.229, 0.224, 0.225])]))
+train_data = datasets.ImageFolder(r'Drive/asl-alphabet-test', transform=transforms.Compose([transforms.RandomResizedCrop(224),transforms.ToTensor()]))
 num_train = len(train_data)
 indices = list(range(num_train))
 np.random.shuffle(indices)
 split = int(np.floor(0.5 * num_train))
 train_idx, valid_idx = indices[split:], indices[:split]
+valid_idx, test_idx = indices[split:], indices[:split]
 train_sampler = SubsetRandomSampler(train_idx)
-test_sampler = SubsetRandomSampler(valid_idx)
+test_sampler = SubsetRandomSampler(test_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=16,sampler = train_sampler)
-test_loader = torch.utils.data.DataLoader(train_data, batch_size=16,sampler = valid_sampler)
+test_loader = torch.utils.data.DataLoader(train_data, batch_size=16,sampler = test_sampler)
+valid_loader = torch.utils.data.DataLoader(train_data, batch_size=16,sampler = valid_sampler)
 
 
 #Importing the model
@@ -34,12 +36,16 @@ optimizer = optim.SGD(vgg16.classifier.parameters(), lr=0.002)
 criterion = nn.CrossEntropyLoss()
 
 #Train Loop
-epochs = 50
-for e in range(epochs):
+def Train(epochs, train_loader, valid_loader):
+  valid_loss_min = np.Inf
+  for epoch in range(epochs):
     train_loss = 0.0
     correct = 0.0
     total = 0.0
+    valid_loss = 0.0
+    vgg16.train()
     for batch_idx, (images, labels) in enumerate(train_loader):
+       
         images = images.cuda()
         labels = labels.cuda()
         output = vgg16(images)
@@ -49,17 +55,35 @@ for e in range(epochs):
         loss.backward()
         optimizer.step()
         train_loss = train_loss + ((1 / (batch_idx + 1)) * (loss.data - train_loss))
-        print(train_loss)
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += np.sum(np.squeeze(pred.eq(labels.data.view_as(pred))).cpu().numpy())
-        total += images.size(0)
-    print(e)
-    print('\nTrain Accuracy: %2d%% (%2d/%2d)' % (
-        100. * correct / total, correct, total))
-    if correct > max_correct:
-        max_correct = correct
-        torch.save(vgg16.state_dict(), 'model_transfer.pt')
-        print('Saving Model... ')  
+    vgg16.eval()
+    for batch_idx, (images, labels) in enumerate(valid_loader):
+      
+      images = images.cuda()
+      
+      labels = labels.cuda()
+      output = vgg16(images)
+      loss = criterion(output, labels)
+      optimizer.zero_grad()
+      valid_loss = valid_loss + ((1 / (batch_idx + 1)) * (loss.data - valid_loss))
+      pred = output.data.max(1, keepdim=True)[1]
+      correct += np.sum(np.squeeze(pred.eq(labels.data.view_as(pred))).cpu().numpy())
+      total += images.size(0)
+    print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(
+            epoch, 
+            train_loss,
+            valid_loss
+            ))
+    if valid_loss <= valid_loss_min:
+            print('Validation loss decreased ({:.6f} --> {:.6f}).  Saving model ...'.format(
+            valid_loss_min,
+            valid_loss))
+            torch.save(vgg16.state_dict(), 'model_cifar.pt')
+            valid_loss_min = valid_loss   
+            print('\nTest Accuracy: %2d%% (%2d/%2d)' % (
+            100. * correct / total, correct, total))
+   
+    
+Train(20, train_loader, valid_loader) 
       
 #Test loop 
 vgg16.load_state_dict(torch.load('model_transfer.pt'))
